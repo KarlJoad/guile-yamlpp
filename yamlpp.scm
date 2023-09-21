db86722605b1bbb3a84609271c19b0ec7872e499
Karl Hallsby <karl@hallsby.com>
2023-09-20 23:31:54 -0500

    The first pass

--- yamlpp.scm
+++ yamlpp.scm
@@ -1,6 +1,38 @@
 (define-module (yamlpp)
   #:export (scm->yaml))
 
+;; (define test '((colors (primary (background . "#000000") (foreground . "#ffffff")) (normal (red . "#ff5f59") (green "#44bc44")))))
+
+(define (yml-atom? v)
+  "YAML atoms are one of either strings, integers, floats, or null."
+  (or (string? v)
+      (number? v)
+      (null? v)))
+
+(define yml-list? list?)
+
+(define (yml-map? k v)
+  (and (symbol? k)
+       (yml-object? v)))
+
+(define (mapping m)
+  ;; TODO: Replace with match-let?
+  (let ((k (car m))
+        (v (cdr m)))
+  (if (and (symbol? k) (yml-atom? v))
+      (format #f "~a: ~a" k v)
+      (map (lambda (sub-v)
+             (mapping sub-v))
+           v))))
+
+(define (yml-object? o)
+  (or (yml-atom? o)
+      (yml-list? o)
+      (yml-map? (car o) (cdr o))))
+
+(define alist->yml (config)
+  (map alist->yml ...))
+
 (define* (scm->yaml scm
                     #:optional (port (current-output-port))
                     #:key (unicode #t))
